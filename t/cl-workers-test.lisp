ff767871a79f19e5fd97568d0bef64d26c9c7de9
garlic0x1 <garlic1@protonmail.com>
2023-11-03 03:36:16 -0500

    more tests and readme

--- t/cl-workers-test.lisp
+++ t/cl-workers-test.lisp
@@ -6,20 +6,20 @@
   :description "Tests for cl-workers")
 (in-suite :cl-workers)
 
+;; ----------------------------------------------------------------------------
 (test :counter
   (let ((result))
     (defworker/global :counter ((i 0)) (increment)
       (incf i increment)
       (setf result i))
-
     (send :counter 1)
     (send :counter 2)
     (send :counter 3)
     (send :counter 4)
     (close-and-join-workers :counter)
-
     (is (= 10 result))))
 
+;; ----------------------------------------------------------------------------
 (test :recursion
   (let ((result))
     (defworker fac () (x ag)
@@ -27,9 +27,26 @@
       (if (= x 0)
           (close-worker self)
           (send self (- x 1) (* x ag))))
-
     (let ((w (fac)))
       (send w 4 1)
       (join-worker w)
-
       (is (= 24 result)))))
+
+;; ----------------------------------------------------------------------------
+(test :waiting
+  (let ((result))
+    (defworker/global :sleepy () () (sleep 1) (setf result :done))
+    (send :sleepy)
+    (close-worker :sleepy)
+    (join-worker :sleepy)
+    (is (eql result :done))))
+
+;; ----------------------------------------------------------------------------
+(test :ordered
+  (let ((result))
+    (defworker/global :in-order () (x) (setf result (cons x result)))
+    (send :in-order 1)
+    (send :in-order 2)
+    (send :in-order 3)
+    (close-and-join-workers :in-order)
+    (is (equal result '(3 2 1)))))
